cd0b36472c39d78b306786a123a3cf88b74b218f
hamidrezakarimi360 <159541723+hamidrezakarimi360@users.noreply.github.com>
2025-08-04 23:51:58 +0330

    Update Lesson.java

--- src/main/java/org/example/Lesson.java
+++ src/main/java/org/example/Lesson.java
@@ -1,4 +1,24 @@
 package org.example;
 
 public class Lesson {
+    private String namebook;
+    private int codebook;
+
+    public Lesson(String namebook, int codebook) {
+        this.namebook = namebook;
+        this.codebook = codebook;
+    }
+
+    String getNamebook() {
+        return namebook;
+    }
+    public void  setNamebook(String namebook) {
+        this.namebook = namebook;
+    }
+    int getCodebook() {
+        return codebook;
+    }
+    public void setCodebook(int codebook) {
+        this.codebook = codebook;
+    }
 }
