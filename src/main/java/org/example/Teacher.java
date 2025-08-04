7240b1d19c6d3ec3e47966d62e006e89d8ec7524
hamidrezakarimi360 <159541723+hamidrezakarimi360@users.noreply.github.com>
2025-08-04 23:52:39 +0330

    Update Teacher.java

--- src/main/java/org/example/Teacher.java
+++ src/main/java/org/example/Teacher.java
@@ -1,4 +1,36 @@
 package org.example;
 
 public class Teacher {
+    private String techername;
+    private String techerlastname;
+    private int techercode;
+    public Teacher(String techername, String techerlastname, int codebook) {
+        this.techername = techername;
+        this.techerlastname = techerlastname;
+        this.techercode = codebook;
+    }
+    public String getTechername() {
+        return techername;
+    }
+
+    public void setTechername(String techername) {
+        this.techername = techername;
+    }
+
+    public String getTecherlastname() {
+        return techerlastname;
+    }
+
+    public void setTecherlastname(String techerlastname) {
+        this.techerlastname = techerlastname;
+
+    }
+
+    public int getTechercode() {
+        return techercode;
+    }
+
+    public void setTechercode(int techercode) {
+        this.techercode = techercode;
+    }
 }
