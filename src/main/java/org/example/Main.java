cb98b12a448ea17e03ccf21fbe31e455e3396dec
hamidrezakarimi360 <159541723+hamidrezakarimi360@users.noreply.github.com>
2025-08-04 23:53:27 +0330

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,95 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class S_tudent {
+    public static void st() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter First Name: ");
+        String firstName = input.nextLine();
+
+        System.out.print("Enter Last Name: ");
+        String lastName = input.nextLine();
+
+        System.out.print("Enter Age: ");
+        int age = Integer.parseInt(input.nextLine());
+
+        System.out.print("Enter Your DateOfBirth(yyyy-MM-dd): ");
+        String dobInput = input.nextLine();
+
+
+        Student student = new Student(firstName, lastName, age, dobInput);
+
+        System.out.println("\nInfo Student:");
+        System.out.println("Name: " + student.getFirstName());
+        System.out.println("LastName: " + student.getLastName());
+        System.out.println("Age: " + student.getAge());
+        System.out.println("Date age: " + student.getDateOfBirth());
+
+        input.close();
+    }
+}
+
+class S_Lesson {
+    public static void on() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Lesson Name: ");
+        String bookname = input.nextLine();
+        System.out.print("Enter Lesson Code: ");
+        int code = input.nextInt();
+        Lesson lesson = new Lesson(bookname, code);
+        System.out.println("\nInfo Lesson:");
+        System.out.println("Name: " + lesson.getNamebook());
+        System.out.println("LastName: " + lesson.getCodebook());
+        input.close();
+    }
+}
+
+class T_teacher {
+    public static void er() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Teacher Name: ");
+        String ferstnameteacher = input.nextLine();
+
+        System.out.print("Enter Teacher Last Name: ");
+        String lastnameteacher = input.nextLine();
+
+        System.out.print("Enter Teacher Code: ");
+        int teachercode = input.nextInt();
+
+        Teacher teacher = new Teacher(ferstnameteacher, lastnameteacher, teachercode);
+
+        System.out.println("\nInfo Teacher:");
+        System.out.println("Name: " + teacher.getTechername());
+        System.out.println("LastName: " + teacher.getTecherlastname());
+        System.out.println("Code: " + teacher.getTechercode());
+
+        input.close();
+
+    }
+
+}
+
+public class mane {
+    public static void main(String[] args) {
+        System.out.println("Select an option");
+        System.out.println("1_Add Student:");
+        System.out.println("2_Add Teacher:");
+        System.out.println("3_Add Lesson:");
+        Scanner input = new Scanner(System.in);
+        int option = input.nextInt();
+        input.nextLine();
+        switch (option) {
+            case 1:
+                S_tudent.st();
+                break;
+            case 2:
+                T_teacher.er();
+                break;
+            case 3:
+                S_Lesson.on();
+                break;
         }
     }
 }
